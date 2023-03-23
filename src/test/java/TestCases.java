64488e6a3a3106f4f433428147cb44fc5e1736f1
LiquidPlummer <kyle.plummer@revature.com>
2023-03-23 16:15:57 -0400

    updating tests

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -8,17 +8,54 @@ public class TestCases {
         SimpleQueue<Integer> queue = new SimpleQueue<>();
 
         Assertions.assertEquals(true, queue.empty());
+    }
 
+    @Test
+    public void testCase2() {
+        SimpleQueue<Integer> queue = new SimpleQueue<>();
         queue.push(1);
 
         Assertions.assertEquals(1, queue.peek());
         Assertions.assertEquals(false, queue.empty());
+    }
 
+    @Test
+    public void testCase3() {
+        SimpleQueue<Integer> queue = new SimpleQueue<>();
+        queue.push(1);
         queue.push(2);
 
+        Assertions.assertEquals(false, queue.empty());
         Assertions.assertEquals(1, queue.pop());
         Assertions.assertEquals(false, queue.empty());
         Assertions.assertEquals(2, queue.pop());
+        Assertions.assertEquals(true, queue.empty());
+    }
+
+    @Test
+    public void testCase4() {
+        SimpleQueue<Integer> queue = new SimpleQueue<>();
+        queue.push(1);
+        queue.push(2);
+
+        Assertions.assertEquals(1, queue.peek());
+        Assertions.assertEquals(1, queue.peek());
+        Assertions.assertEquals(false, queue.empty());
+    }
+
+    @Test
+    public void testCase5() {
+        SimpleQueue<Integer> queue = new SimpleQueue<>();
+
+        queue.push(1);
+        Assertions.assertEquals(1, queue.pop());
+        queue.push(2);
+        Assertions.assertEquals(2, queue.pop());
+        queue.push(1);
+        Assertions.assertEquals(1, queue.pop());
+        queue.push(2);
+        Assertions.assertEquals(2, queue.pop());
+
         Assertions.assertEquals(true, queue.empty());
     }
 
